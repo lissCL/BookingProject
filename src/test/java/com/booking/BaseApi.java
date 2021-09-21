65e9c59fbef75dd9521e44580e5b36da329833f6
cristhain <crisslpj5@gmail.com>
2021-09-21 12:28:34 -0400

    s

--- src/test/java/com/booking/BaseApi.java
+++ src/test/java/com/booking/BaseApi.java
@@ -6,32 +6,30 @@ import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 
 
-public abstract class BaseApi{
+public abstract class BaseApi {
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 
-    public String getToken(){
+    public String getToken() {
         Response response = RestAssured.given().body("{\n" +
                 "    \"username\" : \"admin\",\n" +
                 "    \"password\" : \"password123\"\n" +
                 "}").contentType("application/json")
                 .when().post("/auth");
-        String token= response.path("token");
+        String token = response.path("token");
         return token;
     }
 
-    public int getFirstIdBooking(){
-        String bookingId= RestAssured.given().get("/booking").path("[0].bookingid").toString();
+    public int getFirstIdBooking() {
+        String bookingId = RestAssured.given().get("/booking").path("[0].bookingid").toString();
         return Integer.parseInt(bookingId);
     }
 
-    public String getResponse(int id){
-        String response= RestAssured.given().get("/booking/"+id).asString();
+    public String getResponse(int id) {
+        String response = RestAssured.given().get("/booking/" + id).asString();
         return response;
     }
-
-
 }
