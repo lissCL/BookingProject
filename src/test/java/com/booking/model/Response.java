d37408b2e0a0a09acb65a491eae4d8853db59187
cristhain <crisslpj5@gmail.com>
2021-09-21 16:23:07 -0400

    "ordening class boking and booking dates response responseIterm"

---
+++ src/test/java/com/booking/model/Response.java
@@ -0,0 +1,15 @@
+package com.booking.model;
+
+import java.util.List;
+
+public class Response{
+	private List<ResponseItem> response;
+
+	public void setResponse(List<ResponseItem> response){
+		this.response = response;
+	}
+
+	public List<ResponseItem> getResponse(){
+		return response;
+	}
+}
---
+++ src/test/java/com/booking/model/ResponseItem.java
@@ -0,0 +1,13 @@
+package com.booking.model;
+
+public class ResponseItem{
+	private int bookingid;
+
+	public void setBookingid(int bookingid){
+		this.bookingid = bookingid;
+	}
+
+	public int getBookingid(){
+		return bookingid;
+	}
+}
